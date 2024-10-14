3d16107a99db6b4fcbf7eddeb84a6b839ef8161a
JordanPickles <jordan.l.pickles@gmail.com>
2024-10-14 20:45:28 +0100

    Updates Pitch Image

--- Pitch_Image.py
+++ Pitch_Image.py
@@ -1,10 +1,12 @@
-from mplsoccer import Pitch
+from mplsoccer import Pitch, VerticalPitch
 
 
 def pitch_image():
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='#1e1e1e', line_color='#c7d5cc')
+    pitch = VerticalPitch(pitch_type='statsbomb',corner_arcs=True, half=True, pitch_color='#555555', line_color='#c7d5cc', spot_type='square')
     fig, ax = pitch.draw(figsize=(16, 11), constrained_layout=True, tight_layout=False)
-    fig.set_facecolor('#1e1e1e')
+    fig.set_facecolor('#555555')
+
+
     fig.savefig('/Users/jordanpickles/Library/CloudStorage/OneDrive-Personal/Personal Data Projects/UnderstatData/Football Pitch.png', dpi=300, bbox_inches='tight')
 
 if __name__ == '__main__':
