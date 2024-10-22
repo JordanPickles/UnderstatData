a0723411651ed1e9b6ecdf2d7fd33e912556a98f
JordanPickles <jordan.l.pickles@gmail.com>
2024-10-22 21:05:42 +0100

    Adds Docstrings to Methods

--- Scraper_Script.py
+++ Scraper_Script.py
@@ -1,4 +1,3 @@
-    
 import pandas as pd 
 import understatapi
 import time
@@ -6,9 +5,22 @@ from datetime import datetime
 
 class ShotDataScraper:
     def __init__(self):
+        """
+        Initializes the ShotDataScraper class by creating an Understat API client instance.
+        """
         self.client = understatapi.UnderstatClient()
 
     def match_id_retrieval(self, league_list, season):
+        """
+        Retrieves match IDs for completed matches from the specified leagues and season.
+
+        Parameters:
+        league_list (list): A list of league names (e.g., ['EPL', 'La_Liga']) to retrieve match data from.
+        season (str): The season for which to retrieve match data (e.g., '2024').
+
+        Returns:
+        list: A list of dictionaries containing match IDs and corresponding league names for all past matches.
+        """
         match_id_list = []
         for league in league_list:
             league_matches = self.client.league(league=league).get_match_data(season=season)
@@ -22,6 +34,16 @@ class ShotDataScraper:
         return match_id_list                
 
     def match_shots(self, match_id_list):
+        """
+        Retrieves and processes shot data for a list of match IDs.
+
+        Parameters:
+        match_id_list (list): A list of dictionaries containing match IDs and corresponding league names.
+
+        Returns:
+        DataFrame: A concatenated pandas DataFrame of all shot data for the given matches.
+                   The DataFrame contains shot data for both home and away teams.
+        """
         match_dataframes = []
         for id in match_id_list:
             match_data = self.client.match(id['id']).get_shot_data()
@@ -38,6 +60,12 @@ class ShotDataScraper:
         return df_all_matches
 
     def collect_shot_data(self):
+        """
+        Collects and processes shot data for a predefined list of leagues and season.
+
+        Returns:
+        DataFrame: A pandas DataFrame containing shot data for all the retrieved matches.
+        """
         league_list = ['EPL', 'La_Liga', 'Bundesliga', 'Serie_A', 'Ligue_1']
         season = '2024'
         match_id_list = self.match_id_retrieval(league_list, season)
@@ -46,6 +74,16 @@ class ShotDataScraper:
         return df_shot_data
     
     def clean_shot_data(self, df_shot_data):
+        """
+        Cleans and converts shot data, adjusting positional values and data types to be suitable for Statsbomb pitch images.
+
+        Parameters:
+        df_shot_data (DataFrame): A pandas DataFrame containing shot data to be cleaned.
+
+        Returns:
+        DataFrame: A cleaned DataFrame with X and Y shot positions scaled to field dimensions,
+                  and numerical columns converted to appropriate data types.
+        """
         for col in ['X', 'Y', 'xG']:
             df_shot_data[col] = df_shot_data[col].astype(str).str.replace("'", "", regex=False)
             df_shot_data[col] = df_shot_data[col].astype(float)
